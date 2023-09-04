84cd0465ce0f1c7f5a61bc1e99511e31ea12cdb7
Parth Chaturvedi <parth.chaturvedi@dhyey.com>
2023-09-04 12:55:47 +0530

    first commit

--- my_tennis_club/settings.py
+++ my_tennis_club/settings.py
@@ -76,8 +76,11 @@ WSGI_APPLICATION = 'my_tennis_club.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'MySQL - Flexible Server',
+        'SERVER_NAME':'djangofirstapp12-server',
+        'NAME': 'mydb',
+        'USER': 'eeisdmqmpr',
+        'PASSWORD': 'eeisdmqmpr',
     }
 }
 
